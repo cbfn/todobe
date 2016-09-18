a9368080f88828bcd48d47776f5be4ea952ede07
cnascimentord <christian.nascimento@resultadosdigitais.com.br>
2016-09-18 17:22:31 -0300

    todo

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,11 @@ module Todobe
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.use Rack::Attack
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+    #   end
+    # end
   end
 end
